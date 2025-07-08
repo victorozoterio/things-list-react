7e86f9ac06d0ac9a4789069ac8cbfdf97d100f43
Victor Ozoterio <victorozoterio@gmail.com>
2025-07-08 09:53:23 -0300

    feat: adiciona botão de voltar na página de detalhes da tarefa

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,6 +1,8 @@
-import { useSearchParams } from "react-router-dom";
+import { ChevronLeftIcon } from "lucide-react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function TaskPage() {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
@@ -8,9 +10,17 @@ function TaskPage() {
   return (
     <div className="w-screen h-screen bg-white flex justify-center p-6">
       <div className="w-[500px] space-y-4">
-        <h1 className="text-3xl text-slate-700 font-bold text-center mb-4">
-          Detalhes da Tarefa
-        </h1>
+        <div className="flex justify-center relative mb-4">
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute left-0 top-0 bottom-0 text-slate-700"
+          >
+            <ChevronLeftIcon />
+          </button>
+          <h1 className="text-3xl text-slate-700 font-bold text-center">
+            Detalhes da Tarefa
+          </h1>
+        </div>
 
         <div className="space-y-4 p-6 bg-slate-700 rounded-md flex flex-col">
           <h2 className="text-xl text-white font-bold">{title}</h2>
