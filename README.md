4e1987401da8a1cbb759ca0773412315bd7317bd
Victor Ozoterio <victorozoterio@gmail.com>
2025-07-07 21:21:55 -0300

    feat: adiciona mock de botão para ver detalhes da tarefa

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - Use o comando `npm create vite@5.5.2 .` para criar um projeto react utilizando o vite dentro da pasta que você está;
 - Use o comando `npm install -D tailwindcss@3.4.10 postcss@8.4.41 autoprefixer@10.4.20` para instalar o tailwind;
 - Use o comando `npx tailwindcss init -p` para inicializar o tailwind, em seguida configure o tailwind.config.js;
+- Use o comando `npm install lucide-react@0.435.0` para instalar a biblioteca de ícones da lucide;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,10 +1,16 @@
+import { ChevronRightIcon } from "lucide-react";
+
 function Tasks(props) {
-  console.log(props);
   return (
     <ul className="space-y-4 p-6 bg-slate-700 rounded-md">
       {props.tasks.map((task) => (
-        <li key={task.id} className="bg-slate-500 text-white p-2 rounded-md">
-          {task.title}
+        <li key={task.id} className="flex gap-2">
+          <button className="bg-slate-500 text-left w-full text-white p-2 rounded-md">
+            {task.title}
+          </button>
+          <button className="bg-slate-500 p-2 rounded-md text-white">
+            <ChevronRightIcon />
+          </button>
         </li>
       ))}
     </ul>
